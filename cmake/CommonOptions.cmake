44c1e4dfd33411f690947a6bb2675360ea6634e0
Alexey Rochev <equeim@gmail.com>
2022-01-29 00:02:57 +0300

    CommonOptions.cmake: move variables to function scope

--- cmake/CommonOptions.cmake
+++ cmake/CommonOptions.cmake
@@ -8,47 +8,47 @@ else()
     set(TREMOTESF_QT_VERSION_MAJOR 5)
 endif()
 
-if (MSVC)
-    set(extra_warnings /W4)
-else()
-    set(
-        common_compile_options
-        -Wall
-        -Wextra
-        -Wpedantic
-        -Wnon-virtual-dtor
-        -Wcast-align
-        -Woverloaded-virtual
-        -Wconversion
-        -Wsign-conversion
-        -Wdouble-promotion
-        -Wformat=2
-        -Werror=format
-    )
-    if (NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        list(APPEND common_compile_options -Wlogical-op)
-    endif()
-endif()
-
-set(
-    common_compile_definitions
-    QT_DEPRECATED_WARNINGS
-    QT_DISABLE_DEPRECATED_BEFORE=0x050e00
-)
-
-set(common_public_compile_features cxx_std_17)
-
-if (NOT DEFINED TREMOTESF_SAILFISHOS)
-    message(FATAL_ERROR "TREMOTESF_SAILFISHOS is not defined")
-endif()
-
-if (NOT TREMOTESF_SAILFISHOS)
-    list(APPEND common_compile_definitions QT_MESSAGELOGCONTEXT)
-endif()
-
-set(common_target_properties CXX_EXTENSIONS OFF)
-
 function(set_common_options_on_targets)
+    if (MSVC)
+        set(extra_warnings /W4)
+    else()
+        set(
+            common_compile_options
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wnon-virtual-dtor
+            -Wcast-align
+            -Woverloaded-virtual
+            -Wconversion
+            -Wsign-conversion
+            -Wdouble-promotion
+            -Wformat=2
+            -Werror=format
+        )
+        if (NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+            list(APPEND common_compile_options -Wlogical-op)
+        endif()
+    endif()
+
+    set(
+        common_compile_definitions
+        QT_DEPRECATED_WARNINGS
+        QT_DISABLE_DEPRECATED_BEFORE=0x050e00
+    )
+
+    set(common_public_compile_features cxx_std_17)
+
+    if (NOT DEFINED TREMOTESF_SAILFISHOS)
+        message(FATAL_ERROR "TREMOTESF_SAILFISHOS is not defined")
+    endif()
+
+    if (NOT TREMOTESF_SAILFISHOS)
+        list(APPEND common_compile_definitions QT_MESSAGELOGCONTEXT)
+    endif()
+
+    set(common_target_properties CXX_EXTENSIONS OFF)
+
     get_directory_property(targets BUILDSYSTEM_TARGETS)
     foreach (target ${targets})
         get_target_property(type ${target} TYPE)
