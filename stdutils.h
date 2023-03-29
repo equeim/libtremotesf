6773fb1cf3204cd94bae4595d05ba822a34e633b
Alexey Rochev <equeim@gmail.com>
2023-03-29 22:50:57 +0300

    Use concepts and ranges primitives in stdutils.h

--- stdutils.h
+++ stdutils.h
@@ -5,8 +5,10 @@
 #ifndef LIBTREMOTESF_STDUTILS_H
 #define LIBTREMOTESF_STDUTILS_H
 
-#include <iterator>
+#include <algorithm>
+#include <concepts>
 #include <optional>
+#include <ranges>
 #include <type_traits>
 
 #include <QtGlobal>
@@ -14,96 +16,98 @@
 namespace libtremotesf {
     namespace impl {
         template<typename T>
-        constexpr bool isCheapToCopy =
-            std::is_trivially_copyable_v<std::decay_t<T>> && sizeof(std::decay_t<T>) <= (sizeof(void*) * 2);
+        concept HasReserveFunction = std::ranges::range<T> && requires(T container, std::ranges::range_size_t<T> size) {
+                                                                  container.reserve(size);
+                                                              };
 
         template<typename T>
-        using valueOrConstRef = std::conditional_t<
-            isCheapToCopy<T>,
-            std::decay_t<T>,
-            std::add_lvalue_reference_t<std::add_const_t<std::decay_t<T>>>>;
+        concept HasPushBackFunction =
+            std::ranges::range<T> &&
+            requires(T container, std::ranges::range_reference_t<T> item) { container.push_back(item); };
 
-        template<typename Container, bool IsArray = std::is_array_v<std::remove_reference_t<Container>>>
-        struct ContainerTypes {
-            // Array
-            using size_type = size_t;
-            using value_type = std::remove_all_extents_t<Container>;
-        };
+        template<typename T>
+        concept HasInsertFunction =
+            std::ranges::range<T> &&
+            requires(T container, std::ranges::range_reference_t<T> item) { container.insert(item); };
 
-        template<typename Container>
-        struct ContainerTypes<Container, false> {
-            // Not array
-            using size_type = typename Container::size_type;
-            using value_type = typename Container::value_type;
-        };
+        template<typename T>
+        concept MovableRange = (std::is_rvalue_reference_v<T> && !std::is_const_v<std::remove_reference_t<T>>) ||
+                               (!std::is_reference_v<T> && !std::is_const_v<T>);
+
+        template<std::ranges::range T>
+        using MaybeMovableRangeReference = std::
+            conditional_t<MovableRange<T>, std::ranges::range_rvalue_reference_t<T>, std::ranges::range_reference_t<T>>;
     }
 
-    template<
-        typename Container,
-        typename Size = typename impl::ContainerTypes<Container>::size_type,
-        typename Value = impl::valueOrConstRef<typename impl::ContainerTypes<Container>::value_type>>
-    inline constexpr std::optional<Size> indexOf(const Container& container, Value value) {
-        const auto begin = std::begin(container);
-        const auto end = std::end(container);
-        const auto found = std::find(begin, end, value);
-        if (found == end) {
+    template<std::ranges::random_access_range Range>
+    inline constexpr std::optional<std::ranges::range_size_t<Range>>
+    indexOf(const Range& range, std::ranges::range_value_t<Range> value) {
+        namespace r = std::ranges;
+        const auto found = std::find(r::begin(range), r::end(range), value);
+        if (found == r::end(range)) {
             return std::nullopt;
         };
-        return static_cast<Size>(std::distance(begin, found));
+        return static_cast<r::range_size_t<Range>>(std::distance(r::begin(range), found));
     }
 
-    template<
-        typename Index,
-        typename Container,
-        typename Value = impl::valueOrConstRef<typename impl::ContainerTypes<Container>::value_type>>
-    inline constexpr std::optional<Index> indexOfCasted(const Container& container, Value value) {
-        const auto index = indexOf<Container>(container, value);
+    template<std::integral Index, std::ranges::random_access_range Range>
+    inline constexpr std::optional<Index> indexOfCasted(const Range& range, std::ranges::range_value_t<Range> value) {
+        const auto index = indexOf(range, value);
         if (!index.has_value()) return std::nullopt;
         return static_cast<Index>(*index);
     }
 
-    template<typename NewContainer, typename FromContainer, typename Transform>
-    inline NewContainer createTransforming(FromContainer&& from, Transform&& transform) {
+    template<
+        std::default_initializable NewContainer,
+        std::ranges::forward_range FromRange,
+        std::invocable<impl::MaybeMovableRangeReference<FromRange>> Transform>
+        requires(impl::HasPushBackFunction<NewContainer> || impl::HasInsertFunction<NewContainer>)
+    inline NewContainer createTransforming(FromRange&& from, Transform&& transform) {
         NewContainer container{};
-        container.reserve(static_cast<typename impl::ContainerTypes<NewContainer>::size_type>(std::size(from)));
-        if constexpr (std::is_rvalue_reference_v<decltype(from)> && !std::is_const_v<std::remove_reference_t<decltype(from)>>) {
+        if constexpr (std::ranges::sized_range<FromRange> && impl::HasReserveFunction<NewContainer>) {
+            container.reserve(static_cast<std::ranges::range_size_t<NewContainer>>(std::ranges::size(from)));
+        }
+        auto outputIterator = [&] {
+            if constexpr (impl::HasPushBackFunction<NewContainer>) {
+                return std::back_insert_iterator(container);
+            } else if constexpr (impl::HasInsertFunction<NewContainer>) {
+                return std::insert_iterator(container);
+            }
+        }();
+        if constexpr (impl::MovableRange<FromRange>) {
             std::transform(
-                std::move_iterator(std::begin(from)),
-                std::move_iterator(std::end(from)),
-                std::back_inserter(container),
+                std::move_iterator(std::ranges::begin(from)),
+                std::move_iterator(std::ranges::end(from)),
+                outputIterator,
                 std::forward<Transform>(transform)
             );
         } else {
             std::transform(
-                std::cbegin(from),
-                std::cend(from),
-                std::back_inserter(container),
+                std::ranges::begin(from),
+                std::ranges::end(from),
+                outputIterator,
                 std::forward<Transform>(transform)
             );
         }
         return container;
     }
 
-    template<typename T, typename std::enable_if_t<std::is_floating_point_v<T>, int> = 0>
+    /**
+     * T1 is always deduced as value type, T2 may be a reference
+     */
+    template<std::regular T1, typename T2 = T1>
+        requires(!std::floating_point<T1> && std::same_as<std::remove_const_t<T1>, std::remove_cvref_t<T2>>)
+    inline void setChanged(T1& value, T2&& newValue, bool& changed) {
+        if (newValue != value) {
+            value = std::forward<T2>(newValue);
+            changed = true;
+        }
+    }
+
+    template<std::floating_point T>
     inline void setChanged(T& value, T newValue, bool& changed) {
         if (!qFuzzyCompare(newValue, value)) {
             value = newValue;
-            changed = true;
-        }
-    }
-
-    template<typename T, typename std::enable_if_t<!std::is_floating_point_v<T> && impl::isCheapToCopy<T>, int> = 0>
-    inline void setChanged(T& value, T newValue, bool& changed) {
-        if (newValue != value) {
-            value = newValue;
-            changed = true;
-        }
-    }
-
-    template<typename T, typename std::enable_if_t<!std::is_floating_point_v<T> && !impl::isCheapToCopy<T>, int> = 0>
-    inline void setChanged(T& value, T&& newValue, bool& changed) {
-        if (newValue != value) {
-            value = std::forward<T>(newValue);
             changed = true;
         }
     }
