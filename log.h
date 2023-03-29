943cc215948ed2b076a74ae58d288481b9eb8a58
Alexey Rochev <equeim@gmail.com>
2023-03-29 22:50:57 +0300

    Use concepts in log.h

--- log.h
+++ log.h
@@ -5,14 +5,15 @@
 #ifndef LIBTREMOTESF_LOG_H
 #define LIBTREMOTESF_LOG_H
 
+#include <concepts>
 #include <type_traits>
 
 #include <QMessageLogger>
 #include <QString>
+#include <fmt/core.h>
 
 #include "formatters.h"
 
-#include <fmt/core.h>
 #if FMT_VERSION < 80000
 #    define FORMAT_STRING fmt::string_view
 #else
@@ -20,19 +21,27 @@
 #endif
 
 #if defined(__GNUC__)
-#    define ALWAYS_INLINE [[gnu::always_inline]]
+#    define ALWAYS_INLINE [[gnu::always_inline]] inline
 #elif defined(_MSC_VER)
 #    define ALWAYS_INLINE __forceinline
 #else
-#    define ALWAYS_INLINE
+#    define ALWAYS_INLINE inline
 #endif
 
 namespace libtremotesf {
     namespace impl {
-        template<class T>
-        inline constexpr bool is_exception_v = std::is_base_of_v<std::exception, T>
+        template<typename T>
+        concept IsException = std::derived_from<std::remove_reference_t<T>, std::exception>
 #ifdef Q_OS_WIN
-                                               || std::is_base_of_v<winrt::hresult_error, T>
+                              || std::derived_from<std::remove_reference_t<T>, winrt::hresult_error>
+#endif
+            ;
+
+        template<typename T>
+        concept IsQStringView = std::same_as<std::remove_cvref_t<T>, QStringView>
+#if QT_VERSION_MAJOR >= 6
+                                || std::same_as<std::remove_cvref_t<T>, QUtf8StringView> ||
+                                std::same_as<std::remove_cvref_t<T>, QAnyStringView>
 #endif
             ;
 
@@ -42,73 +51,88 @@ namespace libtremotesf {
             )
                 : type(type), context(fileName, lineNumber, functionName, "default") {}
 
+            /**
+             * Actual log function
+             */
             void log(const QString& string) const;
 
-            ALWAYS_INLINE void log(std::string_view string) const {
-                log(QString::fromUtf8(string.data(), static_cast<QString::size_type>(string.size())));
-            }
-            // Needed to resolve overload resolution ambiguity since QString and std::string_view
-            // are both implicitly convertible from const char*
-            ALWAYS_INLINE void log(const char* string) const { log(std::string_view(string)); };
+            /**
+             * Shortcuts to print strings without going through fmt
+             */
 
-            template<typename... Args, typename = std::enable_if_t<sizeof...(Args) != 0>>
-            void log(FORMAT_STRING fmt, Args&&... args) const {
+            template<std::convertible_to<QString> T>
+            ALWAYS_INLINE void log(const T& string) const {
+                log(QString(string));
+            }
+
+            template<std::convertible_to<std::string_view> T>
+                requires(!std::convertible_to<T, QString>)
+            ALWAYS_INLINE void log(const T& string) const {
+                std::string_view stringView(string);
+                log(QString::fromUtf8(stringView.data(), static_cast<QString::size_type>(stringView.size())));
+            }
+
+            template<IsQStringView T>
+            ALWAYS_INLINE void log(const T& string) const {
+                log(string.toString());
+            }
+
+            /**
+             * Format then print
+             */
+
+            template<typename T>
+            ALWAYS_INLINE void log(const T& value) const {
+                log(singleArgumentFormatString, value);
+            }
+
+            template<typename... Args>
+                requires(sizeof...(Args) != 0)
+            ALWAYS_INLINE void log(FORMAT_STRING fmt, Args&&... args) const {
                 log(fmt::format(fmt, std::forward<Args>(args)...));
             }
 
-            template<
-                typename T,
-                typename =
-                    std::enable_if_t<!std::is_convertible_v<T, QString> && !std::is_convertible_v<T, std::string_view>>>
-            void log(const T& value) const {
-                using Type = std::remove_cv_t<std::remove_reference_t<T>>;
-                if constexpr (
-                    std::is_same_v<Type, QStringView>
-#if QT_VERSION_MAJOR >= 6
-                    || std::is_same_v<Type, QUtf8StringView> || std::is_same_v<Type, QAnyStringView>
-#endif
-                ) {
-                    log(value.toString());
-                } else if constexpr (is_exception_v<Type>) {
-                    logExceptionRecursively<false>(value);
-                } else {
-                    log(singleArgumentFormatString, value);
-                }
+            /**
+             * Special function to print nested exceptions recursively
+             */
+
+            template<IsException T>
+            ALWAYS_INLINE void log(const T& exception) const {
+                logExceptionRecursively<false>(exception);
             }
 
-            template<typename E, typename... Args, typename = std::enable_if_t<sizeof...(Args) != 0>>
-            void logWithException(E&& e, FORMAT_STRING fmt, Args&&... args) const {
-                static_assert(is_exception_v<std::decay_t<E>>, "First argument must be of exception type");
-                log(fmt, std::forward<Args>(args)...);
+            template<IsException E, typename T>
+            ALWAYS_INLINE void logWithException(const E& e, const T& value) const {
+                log(value);
                 logExceptionRecursively<true>(e);
             }
 
-            template<typename E, typename T>
-            void logWithException(E&& e, const T& value) const {
-                static_assert(is_exception_v<std::decay_t<E>>, "First argument must be of exception type");
-                log(value);
+            template<IsException E, typename... Args>
+                requires(sizeof...(Args) != 0)
+            ALWAYS_INLINE void logWithException(const E& e, FORMAT_STRING fmt, Args&&... args) const {
+                log(fmt, std::forward<Args>(args)...);
                 logExceptionRecursively<true>(e);
             }
 
         private:
             template<bool PrintCausedBy>
-            void logExceptionRecursively(const std::exception& e) const {
+            ALWAYS_INLINE void logExceptionRecursively(const std::exception& e) const {
                 return logExceptionRecursivelyImpl<std::exception, PrintCausedBy>(e);
             }
 
             template<bool PrintCausedBy>
-            void logExceptionRecursively(const std::system_error& e) const {
+            ALWAYS_INLINE void logExceptionRecursively(const std::system_error& e) const {
                 return logExceptionRecursivelyImpl<std::system_error, PrintCausedBy>(e);
             }
 
 #ifdef Q_OS_WIN
             template<bool PrintCausedBy>
-            void logExceptionRecursively(const winrt::hresult_error& e) const {
+            ALWAYS_INLINE void logExceptionRecursively(const winrt::hresult_error& e) const {
                 return logExceptionRecursivelyImpl<winrt::hresult_error, PrintCausedBy>(e);
             }
 #endif
 
-            template<typename E, bool PrintCausedBy>
+            template<IsException E, bool PrintCausedBy>
             void logExceptionRecursivelyImpl(const E& e) const;
 
             QtMsgType type;
@@ -140,7 +164,8 @@ namespace libtremotesf {
         fmt::print(stdout, impl::printlnFormatString, value);
     }
 
-    template<typename... Args, typename = std::enable_if_t<sizeof...(Args) != 0>>
+    template<typename... Args>
+        requires(sizeof...(Args) != 0)
     void printlnStdout(FORMAT_STRING fmt, Args&&... args) {
         fmt::print(stdout, impl::printlnFormatString, fmt::format(fmt, std::forward<Args>(args)...));
     }
